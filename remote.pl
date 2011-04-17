1858f393a9da90d2cccbcc118545da22b443f934
Jon McCreery <jwmccreery@gmail.com>
2011-04-17 09:33:49 -0700

    A little bit better, but pretty much just a study in Perl data structures at this point.

--- remote.pl
+++ remote.pl
@@ -1,45 +1,76 @@
 #!/usr/bin/perl
 
+%pools;
 
-@ARGV == 1 or die "I need an arg here..\n";
+
+@ARGV == 1 or die "You gotta gimme a config file to parse... you know this dude...\n";
 
 ($conf) = @ARGV;
 open(CONFFILE, "<", $conf) or die "I can't open the config file you gave me";
 
 while(<CONFFILE>) {
-  $line = $_;
+  my $line = $_;
+
+  # clean up the input stream a bit...
   $line =~ s/#.*$//;
   if($line =~ /^ *$/) { next; }
-# print $line;
 
   # POOL:label:user:pass
-  if($line =~ /^POOL:(.*):(.*):(.*):$/) { 
-    print "P - $1 - $2 - $3\n";
-	my $label, $user, $pass;
-	($label, $user, $pass) = $1,$2,$3;
-	$pools{ $label } = \($user, $pass);
+  if($line =~ /^POOL:~:(.*):~:(.*):~:(.*):~:$/) { 
+#    print "P - $1 - $2 - $3\n";
+	my ($label, $user, $pass) = ($1, $2, $3);
 
-	while ( my ($key, $value) = each(%pools)) {
-	  print "$key => $value\n";
+	my @before   = [];
+	my @after    = [];
+	my @commands = [];
+	my @targets  = [];
+
+	my @pool_attr = ($user, $pass, \@targets, \@before, \@after, \@commands);
+
+	$pools{ $label } = \@pool_attr;
+  }
+
+  # COMMAND:pool:command:when:clean:
+  elsif($line =~ /^COMMAND:~:(.*):~:(.*):~:(.*):~:(.*):~:$/) { 
+#    print "C - $1 - $2 - $3 - $4\n";
+	my ($label, $command, $epoch, $clean) = ($1, $2, $3, $4);
+
+	if($label =~ /ALL/) {
+	  foreach my $pool (keys %pools) {
+	    my $cmd_array_ref = $pools{ $pool }[5];
+		push(@$cmd_array_ref, $command);
+	    $num_cmds = scalar @{$cmd_array_ref};
+	  }
+	} 
+	elsif(exists $pools{$label}) {
+	  my $cmd_array_ref = $pools{ $label }[5];
+	  push(@$cmd_array_ref, $command);
+	  $num_cmds = scalar @{$cmd_array_ref};
 	}
   }
-  # COMMAND:pool:command:when:clean:
-  elsif($line =~ /^COMMAND:(.*):(.*):(.*):(.*):$/) { 
-#   print "C - $1 - $2 - $3 - $4\n";
-  }
+
   # MEMBER:pool:hostname:
-  elsif($line =~ /^MEMBER:(.*):(.*):$/) { 
-#   print "M - $1 - $2\n";
+  elsif($line =~ /^MEMBER:(.*):~:(.*):~:$/) { 
+    my ($label, $user, $pass) = $1, $2, $3;
   }
+
   # PUSH:pool:lpath:rpath:when:
-  elsif($line =~ /^PUSH:(.*):(.*):(.*):(.*):$/) { 
-#   print "H - $1 - $2 - $3 - $4\n";
+  elsif($line =~ /^PUSH:(.*):~:(.*):~:(.*):~:(.*):~:$/) { 
   }
+
   # PULL:pool:lpath:rpath:when:
-  elsif($line =~ /^PULL:(.*):(.*):(.*):(.*):$/) { 
-#   print "L - $1 - $2 - $3 - $4\n";
+  elsif($line =~ /^PULL:(.*):~:(.*):~:(.*):~:(.*):~:$/) { 
   } else {
     print "I didn't recognize this line: $line\n"
   }
+
+
 }
 
+while (my ($key, $value) = each(%pools)) {
+    my $pool_ref = $$value[5];
+	print "I'm seeing " . scalar @$pool_ref . " commands for pool $key:\n";
+	foreach(@$pool_ref) { print "  command for pool $key: " . $_ . "\n"; }
+	print "\n";
+#    print "dump: $key => $cmd\n";
+}
